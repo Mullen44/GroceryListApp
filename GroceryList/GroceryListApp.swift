fbccf8811fc0db546978410ce2263c2fa792a89c
Andrew Mullen <mullenandrew95@gmail.com>
2022-04-02 11:12:47 -0700

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Andrew Mullen on 2022-04-02.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
